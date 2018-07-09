3b4869e64c7762646c333733dd593e7762f4086d
Kyle <kylehawk2@gmail.com>
2018-07-09 12:39:29 -0700

    First Commit

---
+++ Time_Display/apps/Current_Time/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime
+def index(request):
+    context = {
+        'time' : strftime('%Y-%m-%d %H:%M %p', gmtime())
+    }
+    print context
+    return HttpResponse(request, 'Current_Time/index.html', context)
+
+def test(request):
+    response = "Hello I am TEST!"
+    return HttpResponse(response)
